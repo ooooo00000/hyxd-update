5194d993baf4cd9c3bdbb93b36e15c0d5551e543
ooooo00000 <153610955@qq.com>
2018-05-14 01:52:10 +0800

    第一次更新

---
+++ 过强制更新(写死目录).bat
@@ -0,0 +1,10 @@
+@echo off
+:: 请修改为你的荒野行动安装目录
+set dir=C:\Program Files (x86)\hyxd
+rd /s /q "%dir%\BinPatch"
+echo=>"%dir%\BinPatch"
+
+echo=>"%dir%\LocalData\tmp_update_dir"
+
+:: 错误反馈QQ:153610955
+:: 错误反馈:OOOOO00000.UJBLOG.COM
---
+++ 过强制更新(当前目录).bat
@@ -0,0 +1,8 @@
+@echo off
+
+rd /s /q "%cd%\BinPatch"
+echo=>"%cd%\BinPatch"
+echo=>"%cd%\LocalData\tmp_update_dir"
+
+:: 错误反馈QQ:153610955
+:: 错误反馈:OOOOO00000.UJBLOG.COM
