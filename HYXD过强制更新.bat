a2ae2f83a0a44aaa873d724026dd7e0b0da1b0fc
Your Name <you@example.com>
2018-05-14 02:14:11 +0800

    “增加更多文件权限设置”

--- HYXD过强制更新.bat
+++ HYXD过强制更新.bat
@@ -3,6 +3,9 @@
 rd /s /q "%cd%\BinPatch"
 echo=>"%cd%\BinPatch"
 echo=>"%cd%\LocalData\tmp_update_dir"
-
+del  /F /S /Q "%cd%\LocalData\Patch\hyxd"
+echo=>"%cd%\LocalData\Patch\hyxd"
+attrib  "%cd%\LocalData\Patch\hyxd" +r
+attrib  "%cd%\BinPatch" +r
 :: 错误反馈QQ:153610955
 :: 错误反馈:OOOOO00000.UJBLOG.COM
